3066b4595669c0a98b620b37686a30548973c2b6
Andrea Lauro <andrea.lauro94@gmail.com>
2023-05-16 18:59:09 +0200

    main

---
+++ javascript.js
@@ -0,0 +1,28 @@
+//? Crea una funzione che controlli due numeri interi. Ritorna "true" se uno dei due è 50 o se la somma dei due è 50.
+
+// function controlla(n1, n2) {
+    
+//     if (n1 === 50 || n2 === 50) {
+//       return true;
+//     }
+   
+//     if (n1 + n2 === 50) {
+//       return true;
+//     }
+    
+//     return false;
+//   }
+
+// console.log(controlla(22 , 25));
+// console.log(controlla(25, 25));
+
+//? Crea una funzione che rimuova il carattere ad una specifica posizione da una stringa. Passa la stringa e la 
+//? e la poszione come parametri e ritorna la stringa modificata
+
+let array = ["ciao"]
+
+function stringa (array){
+    
+
+
+}
